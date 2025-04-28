d8735b0a16a16839c548a0634d725ec78cc5bd3c
AKO PRECIOUS <akoprecious58@gmail.com>
2025-04-28 16:07:39 -0400

    creating the Meal table

--- resources/js/Components/ApplicationMark.vue
+++ resources/js/Components/ApplicationMark.vue
@@ -1,4 +1,7 @@
-<template>
+
+<script setup>
+import { Head, Link, usePage } from "@inertiajs/vue3";
+</script><template>
   <Link :href="'/'">
        
        <img src="/images/logo.png" class="rounded-full w-14 h-14" alt="">
