ee5cc7144f73201d51bca1b327732aa47b2889ec
AKO PRECIOUS <akoprecious58@gmail.com>
2025-05-02 06:47:14 -0400

    header

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -3,6 +3,6 @@ import { Head, Link, usePage } from "@inertiajs/vue3";
 </script><template>
   <Link :href="'/'">
        
-       <img src="/images/logo.png" class="rounded-full w-14 h-14" alt="">
+       <img src="/images/logo.png" class="rounded-full w-16 h-16" alt="">
          </Link>
 </template>
