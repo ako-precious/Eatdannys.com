91cbe5df7316b36a2fa0e78d3c31445459562a80
AKO PRECIOUS <akoprecious58@gmail.com>
2025-05-05 14:37:14 -0400

    working on the cart

---
+++ resources/js/stores/cart.js
@@ -0,0 +1,19 @@
+import { defineStore } from 'pinia'
+
+export const useCartStore = defineStore('cart', {
+  state: () => ({
+    items: []
+  }),
+  getters: {
+    count: (state) => state.items.length,
+    subtotal: (state) => state.items.reduce((sum, item) => sum + item.price, 0)
+  },
+  actions: {
+    addItem(item) {
+      this.items.push(item)
+    },
+    removeItem(index) {
+      this.items.splice(index, 1)
+    }
+  }
+})
