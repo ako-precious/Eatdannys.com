6b8cf7fd7cc4289e2c83e3c74be162d1371b7900
AKO PRECIOUS <akoprecious58@gmail.com>
2025-05-13 19:00:06 -0400

    stripe

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MealController;
+use App\Http\Controllers\WebhookController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::get('/meal', [MealController::class, 'index']);
+
+// routes/api.php or routes/web.php
+Route::post('/stripe/webhook', [WebhookController::class, 'handleWebhook']);
