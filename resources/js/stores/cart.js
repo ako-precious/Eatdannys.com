304870febeb3731fd2ed1cf4e1be100b0bd2a27f
AKO PRECIOUS <akoprecious58@gmail.com>
2025-05-14 10:51:59 -0400

    save to loaclstorage

--- resources/js/stores/cart.js
+++ resources/js/stores/cart.js
@@ -2,20 +2,28 @@ import { defineStore } from 'pinia'
 
 export const useCartStore = defineStore('cart', {
   state: () => ({
-    items: []
+    items: JSON.parse(localStorage.getItem('cart_items')) || []
   }),
   getters: {
     count: (state) => state.items.length,
     subtotal: (state) =>
-        state.items.reduce((sum, item) => sum + (item.unit_price * item.quantity), 0),
-      
+      state.items.reduce((sum, item) => sum + (item.unit_price * item.quantity), 0)
   },
   actions: {
     addItem(item) {
       this.items.push(item)
+      this.saveToStorage()
     },
     removeItem(index) {
       this.items.splice(index, 1)
+      this.saveToStorage()
+    },
+    clearCart() {
+      this.items = []
+      this.saveToStorage()
+    },
+    saveToStorage() {
+      localStorage.setItem('cart_items', JSON.stringify(this.items))
     }
   }
 })
