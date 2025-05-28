1c3d24e7b7081f44eefcc152594296e854f290cc
AKO PRECIOUS <akoprecious58@gmail.com>
2025-05-28 10:27:21 -0400

    I fixed the toogle for sm screen but i thing it can still be better

--- resources/js/Components/Sidebar/Sidebar.vue
+++ resources/js/Components/Sidebar/Sidebar.vue
@@ -37,24 +37,25 @@
                 <div
                     class="md:min-w-full md:hidden block pb-4 mb-4 "
                 >
-                    <div class="flex flex-wrap">
-                        <div class="w-6/12">
+                    <div class="flex flex-wrap justify-around items-center">
+                        <div class="w-8/12 ">
                             <Link
                                 class="text-left md:pb-2 text-blueGray-600 mr-0 flex justify-center items-center whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
                                 href="/"
                             >
-                                <ApplicationLogo
-                                    class="w-16 h-14"
-                                ></ApplicationLogo>
+                                <img src="/images/logo.png" class="rounded-full w-24 h-24" alt="">
+                                    
+                              
                             </Link>
                         </div>
-                        <div class="w-6/12 flex justify-end">
+                        <div class="w-4/12 flex justify-end">
                             <button
                                 type="button"
-                                class="cursor-pointer text-black opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent"
+                                class="cursor-pointer opacity-80 md:hidden px-3 py-1 text-3xl leading-none bg-transparent rounded border border-solid border-transparent"
                                 v-on:click="toggleCollapseShow('hidden')"
                             >
-                                <i class="fas fa-times"></i>
+                               <font-awesome-icon  class="text-lighred text-3xl"
+                               icon="fa-solid fa-xmark" />
                             </button>
                         </div>
                     </div>
