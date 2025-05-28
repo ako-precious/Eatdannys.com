c779ab0c27bffcef6469538065e1bdd9a730f7e8
AKO PRECIOUS <akoprecious58@gmail.com>
2025-05-28 10:27:21 -0400

    I fixed the toogle for sm screen but i thing it can still be better

--- resources/js/Pages/Profile/Show.vue
+++ resources/js/Pages/Profile/Show.vue
@@ -21,7 +21,7 @@ defineProps({
 
         <div class="flex flex-wrap mt-4">
              <div class="w-full mb-12 px-4">
-            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8 bg-snow shadow-2xl">
+            <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded max-w-7xl mx-auto py-10 sm:px-6 lg:px-8 bg-white">
                 <div v-if="$page.props.jetstream.canUpdateProfileInformation">
                     <UpdateProfileInformationForm :user="$page.props.auth.user" />
 
