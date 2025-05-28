419da3bc2402b81202adf4b525618af1b1dd9164
AKO PRECIOUS <akoprecious58@gmail.com>
2025-05-28 12:30:11 -0400

    adding tokens to the users

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -11,38 +11,14 @@ class OrderController extends Controller
 {
     public function index(Request $request)
     {
-        // $user = Auth::user();
-        // $perPage = $request->get('per_page', 10); // default 10
-
-        // $query = Order::orderBy("id", "desc");
-
-        // // If the user is not an admin, only show their orders
-        // if ($user->role !== 'admin') {
-        //     $query->where('user_id', $user->id);
-        // }
-
-        // $orders = $query->paginate($perPage);
-        // dd(  $orders);
-
-        // $orders = Order::orderBy("id","desc")->paginate(10);
+    
         return  Inertia::render('Order/Index');
     }
     public function getOrder(Request $request)
     {
+//get the user token to continue
 
-        // $user = Auth::user();
-        // $perPage = $request->get('per_page', 10); // default 10
-
-        // $query = Order::orderBy("id", "desc");
-
-        // // If the user is not an admin, only show their orders
-        // if ($user->role !== 'admin') {
-        //     $query->where('user_id', $user->id);
-        // }
-
-        // $orders = $query->paginate($perPage);
-
-          $user = Auth::user();
+        $user = Auth::user();
         $perPage = $request->get('per_page', 10); // default 10
 
         $query = Order::orderBy("id", "desc");
@@ -53,8 +29,6 @@ class OrderController extends Controller
         }
 
         $orders = $query->paginate($perPage);
-        dd(  $orders);
-
        
         return response()->json([
             'orders' => $orders,
