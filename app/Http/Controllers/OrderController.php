07359e3afeab09b845e141e8bb2f2ca720b86ffa
AKO PRECIOUS <akoprecious58@gmail.com>
2025-05-28 11:47:46 -0400

    forget how to access protected api lol

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,24 +5,59 @@ namespace App\Http\Controllers;
 use App\Models\Order;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
-
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        // $user = Auth::user();
+        // $perPage = $request->get('per_page', 10); // default 10
+
+        // $query = Order::orderBy("id", "desc");
+
+        // // If the user is not an admin, only show their orders
+        // if ($user->role !== 'admin') {
+        //     $query->where('user_id', $user->id);
+        // }
+
+        // $orders = $query->paginate($perPage);
+        // dd(  $orders);
+
         // $orders = Order::orderBy("id","desc")->paginate(10);
         return  Inertia::render('Order/Index');
     }
-
     public function getOrder(Request $request)
     {
+
+        // $user = Auth::user();
+        // $perPage = $request->get('per_page', 10); // default 10
+
+        // $query = Order::orderBy("id", "desc");
+
+        // // If the user is not an admin, only show their orders
+        // if ($user->role !== 'admin') {
+        //     $query->where('user_id', $user->id);
+        // }
+
+        // $orders = $query->paginate($perPage);
+
+          $user = Auth::user();
         $perPage = $request->get('per_page', 10); // default 10
-$orders = Order::orderBy("id", "desc")->paginate($perPage);
+
+        $query = Order::orderBy("id", "desc");
+
+        // If the user is not an admin, only show their orders
+        if ($user->role !== 'admin') {
+            $query->where('user_id', $user->id);
+        }
+
+        $orders = $query->paginate($perPage);
+        dd(  $orders);
+
+       
         return response()->json([
             'orders' => $orders,
-            // 'requires_password_setup' => $user->wasRecentlyCreated = true ,
         ]);
-       
     }
 }
