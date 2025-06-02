2709b0b65c48963afe04fc78be51b376633e12a9
AKO PRECIOUS <akoprecious58@gmail.com>
2025-06-02 19:33:51 -0400

    work on editing

--- app/Http/Controllers/MealController.php
+++ app/Http/Controllers/MealController.php
@@ -42,4 +42,49 @@ public function getMeals(Request $request)
         return  Inertia::render('Meals/Index');
     }
     
+      public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        
+         return  Inertia::render('Meals/Edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
 }
