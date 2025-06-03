cc2df35af4195d3399b8d743e4d9ba5bbd07db67
AKO PRECIOUS <akoprecious58@gmail.com>
2025-06-02 23:24:23 -0400

    adding photos

--- app/Models/Meal.php
+++ app/Models/Meal.php
@@ -5,20 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 class Meal extends Model
 {
-    
+
     use HasFactory;
     protected $table = 'meals';
     protected $fillable = ['category_id', 'name', 'description', 'price'];
 
     protected $casts = [
         'prices' => 'array',
-    ];    
-    
-    public function category(){
+    ];
 
-    return $this->belongsTo(Category::class);
-}
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 
+    public function photos()
+    {
+        return $this->hasMany(MealPhoto::class);
+    }
 }
