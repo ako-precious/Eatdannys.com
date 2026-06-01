a22e552db28c3c5790c5982c858e1d62b984b407
AKO PRECIOUS <akoprecious58@gmail.com>
2026-06-01 16:55:13 -0400

    Add GSAP library for enhanced animations and update Meal.vue button styles

--- resources/js/Pages/Meal.vue
+++ resources/js/Pages/Meal.vue
@@ -4,23 +4,23 @@
         <!-- Navigation tabs -->
         <div class="flex gap-4 mb-8">
             <Link :href="route('dining')">
-                <button class="tab-btn group">
+                <button class="relative cursor-pointer py-3 px-6 inline-flex justify-center text-sm uppercase rounded-lg border-solid transition-transform duration-300 ease-in-out overflow-hidden group">
                     <span class="relative z-20 font-semibold text-oynx dark:text-snow">Dine In</span>
-                    <span class="tab-shine"></span>
-                    <span class="tab-border-tl"></span>
-                    <span class="tab-border-tr group-hover:h-[90%]"></span>
-                    <span class="tab-border-bl group-hover:h-[90%]"></span>
-                    <span class="tab-border-br"></span>
+                    <span class="absolute left-[-75%] top-0 h-full w-[50%] bg-oynx/20 rotate-12 z-10 blur-lg group-hover:left-[125%] transition-all duration-1000 ease-in-out"></span>
+                    <span class="w-1/2 transition-all duration-300 block border-[#D4EDF9] absolute h-[20%] rounded-tl-lg border-l-2 border-t-2 top-0 left-0"></span>
+                    <span class="w-1/2 transition-all duration-300 block border-[#D4EDF9] absolute h-[60%] group-hover:h-[90%] rounded-tr-lg border-r-2 border-t-2 top-0 right-0"></span>
+                    <span class="w-1/2 transition-all duration-300 block border-[#D4EDF9] absolute h-[60%] group-hover:h-[90%] rounded-bl-lg border-l-2 border-b-2 left-0 bottom-0"></span>
+                    <span class="w-1/2 transition-all duration-300 block border-[#D4EDF9] absolute h-[20%] rounded-br-lg border-r-2 border-b-2 right-0 bottom-0"></span>
                 </button>
             </Link>
             <Link :href="route('dining&takeout')">
-                <button class="tab-btn group">
+                <button class="relative cursor-pointer py-3 px-6 inline-flex justify-center text-sm uppercase rounded-lg border-solid transition-transform duration-300 ease-in-out overflow-hidden group">
                     <span class="relative z-20 font-semibold text-oynx dark:text-snow">Bulk Order</span>
-                    <span class="tab-shine"></span>
-                    <span class="tab-border-tl"></span>
-                    <span class="tab-border-tr group-hover:h-[90%]"></span>
-                    <span class="tab-border-bl group-hover:h-[90%]"></span>
-                    <span class="tab-border-br"></span>
+                    <span class="absolute left-[-75%] top-0 h-full w-[50%] bg-oynx/20 rotate-12 z-10 blur-lg group-hover:left-[125%] transition-all duration-1000 ease-in-out"></span>
+                    <span class="w-1/2 transition-all duration-300 block border-[#D4EDF9] absolute h-[20%] rounded-tl-lg border-l-2 border-t-2 top-0 left-0"></span>
+                    <span class="w-1/2 transition-all duration-300 block border-[#D4EDF9] absolute h-[60%] group-hover:h-[90%] rounded-tr-lg border-r-2 border-t-2 top-0 right-0"></span>
+                    <span class="w-1/2 transition-all duration-300 block border-[#D4EDF9] absolute h-[60%] group-hover:h-[90%] rounded-bl-lg border-l-2 border-b-2 left-0 bottom-0"></span>
+                    <span class="w-1/2 transition-all duration-300 block border-[#D4EDF9] absolute h-[20%] rounded-br-lg border-r-2 border-b-2 right-0 bottom-0"></span>
                 </button>
             </Link>
         </div>
@@ -288,23 +288,3 @@ export default {
 };
 </script>
 
-<style scoped>
-.tab-btn {
-    @apply relative cursor-pointer py-3 px-6 font-barlow inline-flex justify-center text-sm uppercase rounded-lg border-solid transition-transform duration-300 ease-in-out overflow-hidden;
-}
-.tab-shine {
-    @apply absolute left-[-75%] top-0 h-full w-[50%] bg-oynx/20 rotate-12 z-10 blur-lg group-hover:left-[125%] transition-all duration-1000 ease-in-out;
-}
-.tab-border-tl {
-    @apply w-1/2 drop-shadow-3xl transition-all duration-300 block border-[#D4EDF9] absolute h-[20%] rounded-tl-lg border-l-2 border-t-2 top-0 left-0;
-}
-.tab-border-tr {
-    @apply w-1/2 drop-shadow-3xl transition-all duration-300 block border-[#D4EDF9] absolute h-[60%] rounded-tr-lg border-r-2 border-t-2 top-0 right-0;
-}
-.tab-border-bl {
-    @apply w-1/2 drop-shadow-3xl transition-all duration-300 block border-[#D4EDF9] absolute h-[60%] rounded-bl-lg border-l-2 border-b-2 left-0 bottom-0;
-}
-.tab-border-br {
-    @apply w-1/2 drop-shadow-3xl transition-all duration-300 block border-[#D4EDF9] absolute h-[20%] rounded-br-lg border-r-2 border-b-2 right-0 bottom-0;
-}
-</style>
