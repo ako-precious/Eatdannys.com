6bfb3e89c9e05eadf9141983b3d4164de451d95d
AKO PRECIOUS <akoprecious58@gmail.com>
2026-06-02 01:10:52 -0400

    Refactor Header components to streamline structure and improve navigation functionality

--- resources/js/Pages/Header/Index.vue
+++ resources/js/Pages/Header/Index.vue
@@ -1,223 +1,27 @@
 <script setup>
-import { onMounted } from "vue";
-import { Head, Link, usePage } from "@inertiajs/vue3";
-import DropBarNav from "./DropBarNav.vue";
-import Navbar from "./Navbar.vue";
-import Dropdown from "@/Components/Dropdown.vue";
-import CartIcon from "./CartIcon.vue";
-import axios from "axios";
-// import { route } from 'ziggy-js';
+import Navbar from './Navbar.vue'
+import DropBarNav from './DropBarNav.vue'
+import CartIcon from './CartIcon.vue'
 
 defineProps({
-    canLogin: Boolean,
-    canRegister: Boolean,
-    laravelVersion: String,
-    phpVersion: String,
-    pushSub: Boolean,
-});
-</script>
-
-<script>
-export default {
-    inheritAttrs: false,
-
-    data() {
-        return {
-            // meals: [],
-            // page: 1, // Current page
-            // perPage: 12, // Number of items per page
-            // hasMoreData: true,
-            isHeaderFixed: false,
-        };
-    },
-    beforeDestroy() {
-        window.removeEventListener("scroll", this.handleScroll);
-    },
-    computed: {
-        // isSpecialOrderRoute() {
-        //   return route().current('special-order');
-        // },
-        // isBulkRoute() {
-        //   return route().current('bulk-meal');
-        // },
-        // isWelcome() {
-        //   return route().current('welcome');
-        // },
-    },
-
-    mounted() {
-        window.addEventListener("scroll", this.handleScroll);
-        // if (this.$page.props.auth.user) {
-        //     if (
-        //         this.$page.props.auth.user.phone_number == null ||
-        //         this.$page.props.auth.user.address == null
-        //     ) {
-        //         this.$inertia.visit(`/user/profile`);
-        //     }
-        // }
-    },
-    created() {
-        this.handleScroll();
-    },
-    methods: {
-        handleScroll() {
-            // Adjust the scroll threshold as needed
-            const scrollThreshold = 50;            
-            this.isHeaderFixed = window.scrollY > scrollThreshold;
-        },
-    },
-};
+  canLogin:      Boolean,
+  canRegister:   Boolean,
+  laravelVersion: String,
+  phpVersion:    String,
+  pushSub:       Boolean,
+})
 </script>
 
 <template>
-    <div class="  ">
-       
-        <header
-            :class="{
-                'fix align-bottom shadow-sm py-4 px-8 bg-snow dark:bg-oynx text-oynx dark:text-snow': isHeaderFixed,
-            }"
-            class="py-5  z-10 transition-all duration-300 delay-75 ease-in animate-fade-in"
-        >
-            <Navbar class="">
-                <template #search>
-                    <div
-                        class="w-full p-4 max-w-sm lg:max-w-lg 2xl:max-w-2xl rounded-md hidden md:flex items-center"
-                    >
-                        <div
-                            v-if="!isHeaderFixed"
-                            class="bg-transparent capitalize font-bold text-sm mr-4 flex justify-around w-full transition-all duration-300 delay-75 ease-in"
-                            name=""
-                            id=""
-                        >
-                            <Link
-                                class="py-2 px-3 navbar-link"
-                                :href="route('welcome')"
-                            >
-                            <p class="font-bold two">
-                                <span class="hidden text-nowrap lg:block">dining  & take out</span>
-                                <span  class="lg:hidden block">dining...</span>
-                                <span
-                                    class="absolute bottom-1 left-1/2 w-0 transition-all h-1 bg-polynesian duration-250 ease-in"
-                                ></span>
-                                <span
-                                    class="absolute bottom-1 right-1/2 w-0 transition-all h-1 bg-polynesian duration-250 ease-in"
-                                ></span></p
-                        ></Link>
-                               
-                            <Link
-                                class="py-2 px-3 navbar-link"
-                                :href="route('catering')"
-                            >
-                                <p class="font-bold two">
-                                    <span>Catering </span>
-                                    <span
-                                        class="absolute bottom-1 left-1/2 w-0 transition-all h-1 bg-polynesian duration-250 ease-in"
-                                    ></span>
-                                    <span
-                                        class="absolute bottom-1 right-1/2 w-0 transition-all h-1 bg-polynesian duration-250 ease-in"
-                                    ></span>
-                                </p>
-                            </Link>
-                           
-                          
-                            
-                            <Link
-                                class="py-2 px-3 navbar-link"
-                                :href="route('bakery')"><p class="font-bold two">
-                                    <span>Bakery</span>
-                                    <span
-                                        class="absolute bottom-1 left-1/2 w-0 transition-all h-1 bg-polynesian duration-250 ease-in"
-                                    ></span>
-                                    <span
-                                        class="absolute bottom-1 right-1/2 w-0 transition-all h-1 bg-polynesian duration-250 ease-in"
-                                    ></span>
-                                </p>
-                            </Link>
-                        </div>
-                        <div
-                            v-else="isHeaderFixed"
-                            class="bg-transparent capitalize font-bold text-sm mr-4 flex justify-around w-full transition-all duration-300 delay-75 ease-in"
-                            name=""
-                            id=""
-                        >
-                            
-                           <Link
-                                class="py-2 px-3 navbar-link text-oynx"
-                                :href="route('dining&takeout')"
-                                ><p class="font-bold two">
-                                    <span class="hidden text-nowrap lg:block">dining  & take out</span>
-                                    <span  class="lg:hidden block">dining...</span>
-                                    <span
-                                        class="absolute bottom-1 left-1/2 w-0 transition-all h-1 bg-polynesian duration-250 ease-in"
-                                    ></span>
-                                    <span
-                                        class="absolute bottom-1 right-1/2 w-0 transition-all h-1 bg-polynesian duration-250 ease-in"
-                                    ></span></p
-                            ></Link>
-                            <Link
-                                class="py-2 px-3 navbar-link text-oynx!"
-                                :href="route('welcome')"
-                                >
-                                <p class="font-bold two">
-                                    <span>Catering </span>
-                                    <span
-                                    class="absolute bottom-1 left-1/2 w-0 transition-all h-1 bg-polynesian duration-250 ease-in"
-                                    ></span>
-                                    <span
-                                    class="absolute bottom-1 right-1/2 w-0 transition-all h-1 bg-polynesian duration-250 ease-in"
-                                    ></span>
-                                </p>
-                            </Link>
-                           
-                          
-                            
-                            <Link
-                                class="py-2 px-3 navbar-link text-oynx"
-                                :href="route('bakery')"><p class="font-bold two">
-                                    <span>Bakery</span>
-                                    <span
-                                        class="absolute bottom-1 left-1/2 w-0 transition-all h-1 bg-polynesian duration-250 ease-in"
-                                    ></span>
-                                    <span
-                                        class="absolute bottom-1 right-1/2 w-0 transition-all h-1 bg-polynesian duration-250 ease-in"
-                                    ></span>
-                                </p>
-                            </Link>
-                        </div>
-                        <slot name="search-content"></slot>
-                    </div>
-                </template>
-                <template #dropdown>
-                    
-                   
-                    <DropBarNav class="cursor-pointer "
-                    :canLogin="canLogin"
-                    :canRegister="canRegister"
-                    :laravelVersion="laravelVersion"
-                    :phpVersion="phpVersion"
-                    />
-                    <button @click="openCart">
-  <CartIcon />
-</button>
-                  
-                    
-                </template>
-            </Navbar>
-            <slot name="extra-content"></slot>
-        </header>
-    </div>
+  <Navbar>
+    <template #dropdown>
+      <DropBarNav
+        :canLogin="canLogin"
+        :canRegister="canRegister"
+        :laravelVersion="laravelVersion"
+        :phpVersion="phpVersion"
+      />
+      <CartIcon />
+    </template>
+  </Navbar>
 </template>
-
-<style scoped>
-.fix {
-    position: fixed;
-    top: 0;
-    right: 0;
-    width: 100%;
-    padding: 0.4rem 2rem;
-    z-index: 1000; /* Adjust z-index as needed */
-}
-.two:hover span {
-    width: 50%;
-}
-</style>
--- resources/js/Pages/Header/Navbar.vue
+++ resources/js/Pages/Header/Navbar.vue
@@ -1,124 +1,255 @@
-<template>
-    <div
-        class="container mx-auto px-4 lg:px-10 flex justify-between items-center "
-    >
-        <!-- logo -->
-        <div class="md:w-48 flex-shrink-0 flex items-center">
-            
-             <!-- Logo -->
-             <Link :href="route('welcome')">
-             <div class="w-full flex items-center">
-                    <ApplicationMark class="block " />
-                    <!-- <span
-                        class="hidden md:block ml-2 font-bold transition-all duration-200 ease-in-out text-2xl uppercase text-oynx dark:text-snow"
-                    >
-                        Danny's
-                    </span> -->
-                    
-                </div>
-            </Link>
-               
-        </div>
-        
-        <!-- search -->
-        <slot name="search"></slot>
-
-        <!-- buttons -->
-        <nav class="contents">
-            <ul class="ml-4 xl:w-48 flex items-center justify-endrounded-full">
-              
-                  <slot name="dropdown"> </slot>
-                <div class="md:hidden px-4 flex items-center">
-                    <a
-                        mini-sidenav-burger=""
-                        href="javascript:;"
-                        class="sm uppercase:marker:hidden p-0 text-sm uppercase text-white transition-all ease-nav-brand block"
-                        aria-expanded="false"
-                    >
-                        <Dropdown align="right" width="48">
-                            <template #trigger>
-                                <div class="w-4.5 overflow-hidden">
-                                    <i
-                                        class="ease mb-0.75 relative block h-0.5 rounded-sm uppercase bg-persian group-hover:bg-polynesian dark:group-hover:bg-lighred transition-all"
-                                    ></i>
-                                    <i
-                                        class="ease mb-0.75 relative block h-0.5 rounded-sm uppercase bg-persian group-hover:bg-polynesian dark:group-hover:bg-lighred transition-all"
-                                    ></i>
-                                    <i
-                                        class="ease relative block h-0.5 rounded-sm uppercase bg-persian group-hover:bg-polynesian dark:group-hover:bg-lighred transition-all"
-                                    ></i>
-                                </div>
-                            </template>
-
-                            <template #content class="bg-snow dark:bg-oynx">
-                                <div
-                                    class="origin-top-right absolute right-0 mt-2 w-48 delay-75 rounded-md border-snow ring-1 hover:shadow-snow-sm uppercase w-full  bg-snow dark:bg-oynx   z-20 transition-all duration-250 ease-in dark:border-oynx"
-                                >
-                                   <DropdownLink
-                                         :href="route('welcome')"
-                                        class="relative flex items-center rounded-md py-2 text-sm text-oynx hover:text-polynesian dark:text-snow dark:hover:text-lighred cursor-pointer transition-all duration-200 ease-in-out text-nowrap w-full"
-                                    >
-                                        <p class="w-full">dining & take out</p>
-                                    </DropdownLink>
-                                    <DropdownLink
-                                           :href="route('catering')"
-                                        class="relative flex items-center rounded-md  py-2 text-sm text-oynx hover:text-polynesian dark:text-snow dark:hover:text-lighred cursor-pointer transition-all duration-200 ease-in-out text-nowrap w-full"
-                                    >
-                                        <p class="w-full">Event Catering</p>
-                                    </DropdownLink>
-                                   
-                                   
-                                    <DropdownLink
-                                          :href="route('bakery')"
-                                        class="relative flex items-center rounded-md py-2 text-sm text-oynx hover:text-polynesian dark:text-snow dark:hover:text-lighred cursor-pointer transition-all duration-200 ease-in-out text-nowrap w-full"
-                                    >
-                                        
-                                            <p class="w-full">bakery</p>
-                                        
-                                    </DropdownLink>
-                                </div>
-                             
-                            </template>
-                        </Dropdown>
-                    </a>
-                </div>
-              
-                
-                <!-- <li  -->
-            </ul>
-        </nav>
-    </div>
-</template>
-
 <script setup>
-import { Link } from "@inertiajs/vue3";
-import ApplicationMark from '@/Components/ApplicationMark.vue';
-// import route from "vendor/tightenco/ziggy/src/js";
+import { ref, onMounted, onUnmounted, nextTick } from 'vue'
+import { Link } from '@inertiajs/vue3'
+import { gsap } from 'gsap'
 
-import DropdownLink from "@/Components/DropdownLink.vue";
-import Dropdown from "@/Components/Dropdown.vue";
-</script>
+// ─── Nav links ────────────────────────────────────────────────────────────────
+const navLinks = [
+  { label: 'Dining & Takeout', route: 'welcome' },
+  { label: 'Event Catering',   route: 'catering' },
+  { label: 'Bakery',           route: 'bakery' },
+]
 
-<script>
-import axios from "axios";
-export default {
-    data() {
-        return {
-          
-        };
-    },
-    created() {
-         },
-   
-    methods: {
-     
-    },
-};
-</script>
-<style>
-@media (prefers-color-scheme: dark) {
-    .dark\:bg-dots-lighter {
-        background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(255,255,255,0.07)'/%3E%3C/svg%3E");
-    }
+const isActive = (routeName) => {
+  try { return route().current(routeName) } catch { return false }
 }
-</style>
+
+// ─── Refs ─────────────────────────────────────────────────────────────────────
+const navEl         = ref(null)
+const mobileMenuEl  = ref(null)
+const mobileItemEls = ref([])
+const mobileOpen    = ref(false)
+const isScrolled    = ref(false)
+
+// ─── Scroll: transparent ↔ solid + hide-on-down / show-on-up ─────────────────
+let lastY  = 0
+let hidden = false
+
+const onScroll = () => {
+  const y = window.scrollY
+  isScrolled.value = y > 48
+
+  if (navEl.value && y > 280) {
+    if (y > lastY && !hidden) {
+      gsap.to(navEl.value, { y: '-110%', duration: 0.38, ease: 'power3.inOut' })
+      hidden = true
+    } else if (y < lastY && hidden) {
+      gsap.to(navEl.value, { y: '0%', duration: 0.38, ease: 'power3.out' })
+      hidden = false
+    }
+  }
+  lastY = y
+}
+
+// ─── Mobile menu ──────────────────────────────────────────────────────────────
+const openMobile = async () => {
+  mobileItemEls.value = []
+  mobileOpen.value = true
+  document.body.style.overflow = 'hidden'
+  await nextTick()
+
+  gsap.fromTo(
+    mobileMenuEl.value,
+    { autoAlpha: 0 },
+    { autoAlpha: 1, duration: 0.32, ease: 'power2.out' }
+  )
+
+  const items = mobileItemEls.value.filter(Boolean)
+  if (items.length) {
+    gsap.fromTo(
+      items,
+      { y: 36, autoAlpha: 0 },
+      { y: 0, autoAlpha: 1, duration: 0.55, stagger: 0.07, ease: 'power3.out', delay: 0.12 }
+    )
+  }
+}
+
+const closeMobile = () => {
+  const items = [...mobileItemEls.value].filter(Boolean).reverse()
+  gsap.timeline({
+    onComplete: () => {
+      mobileOpen.value = false
+      document.body.style.overflow = ''
+    },
+  })
+    .to(items, { y: -18, autoAlpha: 0, duration: 0.22, stagger: 0.04, ease: 'power2.in' })
+    .to(mobileMenuEl.value, { autoAlpha: 0, duration: 0.22, ease: 'power2.in' }, '-=0.1')
+}
+
+// ─── Lifecycle ────────────────────────────────────────────────────────────────
+onMounted(() => {
+  lastY = window.scrollY
+  isScrolled.value = lastY > 48
+
+  gsap.fromTo(
+    navEl.value,
+    { y: -56, autoAlpha: 0 },
+    { y: 0, autoAlpha: 1, duration: 0.72, ease: 'power3.out', delay: 0.08 }
+  )
+
+  window.addEventListener('scroll', onScroll, { passive: true })
+})
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', onScroll)
+  document.body.style.overflow = ''
+})
+</script>
+
+<template>
+  <!-- ── Fixed navbar ────────────────────────────────────────────────────── -->
+  <header
+    ref="navEl"
+    class="fixed top-0 left-0 right-0 z-[990] transition-colors duration-500"
+    :class="isScrolled
+      ? 'bg-oynx/96 backdrop-blur-md border-b border-snow/8'
+      : 'bg-transparent'"
+    style="will-change: transform"
+  >
+    <div class="max-w-7xl mx-auto px-6 lg:px-10">
+      <div class="flex items-center justify-between h-16 lg:h-[72px]">
+
+        <!-- Logo -->
+        <Link
+          :href="route('welcome')"
+          class="flex-shrink-0 flex items-center gap-2.5 group"
+          aria-label="Danny's — Home"
+        >
+          <img
+            src="/images/logo.png"
+            alt="Danny's"
+            class="w-8 h-8 rounded-full object-cover ring-1 ring-snow/20 transition-all duration-300 group-hover:ring-persian/60 group-hover:scale-105"
+          />
+          <span class="hidden sm:block font-bold text-snow text-sm tracking-widest uppercase">
+            Danny's
+          </span>
+        </Link>
+
+        <!-- Desktop nav links (centre) -->
+        <nav class="hidden md:flex items-center gap-8" aria-label="Primary navigation">
+          <Link
+            v-for="link in navLinks"
+            :key="link.route"
+            :href="route(link.route)"
+            class="nav-link group relative text-xs font-bold uppercase tracking-[0.16em] py-1 transition-colors duration-200 focus:outline-none focus-visible:text-persian"
+            :class="isActive(link.route) ? 'text-persian' : 'text-snow/70 hover:text-snow'"
+          >
+            {{ link.label }}
+            <!-- Animated underline indicator -->
+            <span
+              class="absolute -bottom-px left-0 h-px bg-persian transition-all duration-300 ease-out origin-left"
+              :class="isActive(link.route) ? 'w-full' : 'w-0 group-hover:w-full'"
+              aria-hidden="true"
+            ></span>
+          </Link>
+        </nav>
+
+        <!-- Right side: user menu + cart passed from parent, plus mobile trigger -->
+        <div class="flex items-center gap-2">
+          <slot name="dropdown" />
+
+          <!-- Mobile hamburger button -->
+          <button
+            class="md:hidden flex flex-col items-center justify-center gap-[5px] w-9 h-9 rounded-lg hover:bg-snow/10 transition-all duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-persian"
+            :aria-expanded="mobileOpen"
+            aria-label="Open navigation menu"
+            @click="openMobile"
+          >
+            <span class="block w-5 h-[1.5px] bg-snow rounded-full transition-all duration-300"></span>
+            <span class="block w-3.5 h-[1.5px] bg-snow rounded-full transition-all duration-300"></span>
+            <span class="block w-5 h-[1.5px] bg-snow rounded-full transition-all duration-300"></span>
+          </button>
+        </div>
+
+      </div>
+    </div>
+
+    <!-- Hairline border that fades in when scrolled -->
+    <div
+      class="absolute inset-x-0 bottom-0 h-px bg-snow/8 transition-opacity duration-500"
+      :class="isScrolled ? 'opacity-100' : 'opacity-0'"
+      aria-hidden="true"
+    ></div>
+  </header>
+
+  <!-- ── Mobile fullscreen overlay ─────────────────────────────────────── -->
+  <Teleport to="body">
+    <div
+      v-if="mobileOpen"
+      ref="mobileMenuEl"
+      class="fixed inset-0 z-[999] flex flex-col bg-oynx/98 backdrop-blur-2xl"
+      style="opacity: 0; visibility: hidden"
+      role="dialog"
+      aria-modal="true"
+      aria-label="Navigation menu"
+    >
+      <!-- Top bar -->
+      <div class="flex items-center justify-between px-6 py-5 border-b border-snow/8">
+        <Link
+          :href="route('welcome')"
+          class="flex items-center gap-2.5 group"
+          @click="closeMobile"
+        >
+          <img
+            src="/images/logo.png"
+            alt="Danny's"
+            class="w-8 h-8 rounded-full object-cover"
+          />
+          <span class="font-bold text-snow text-sm tracking-widest uppercase">Danny's</span>
+        </Link>
+
+        <button
+          class="w-10 h-10 flex items-center justify-center rounded-full text-snow/55 hover:text-snow hover:bg-snow/10 transition-all duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-persian"
+          aria-label="Close navigation menu"
+          @click="closeMobile"
+        >
+          <i class="fa-solid fa-xmark text-xl" aria-hidden="true"></i>
+        </button>
+      </div>
+
+      <!-- Nav links (large) -->
+      <nav
+        class="flex-1 flex flex-col justify-center px-8"
+        aria-label="Mobile navigation"
+      >
+        <Link
+          v-for="(link, i) in navLinks"
+          :key="link.route"
+          :ref="el => { if (el) mobileItemEls[i] = el }"
+          :href="route(link.route)"
+          class="group flex items-center justify-between py-5 border-b border-snow/8 focus:outline-none"
+          :class="isActive(link.route) ? 'text-persian' : 'text-snow'"
+          style="opacity: 0"
+          @click="closeMobile"
+        >
+          <span
+            class="text-3xl sm:text-4xl font-black uppercase tracking-tight transition-colors duration-200 group-hover:text-persian"
+          >
+            {{ link.label }}
+          </span>
+          <i
+            class="fa-solid fa-arrow-right text-sm opacity-0 -translate-x-2 transition-all duration-200 group-hover:opacity-100 group-hover:translate-x-0"
+            aria-hidden="true"
+          ></i>
+        </Link>
+      </nav>
+
+      <!-- Footer contact -->
+      <div
+        :ref="el => { if (el) mobileItemEls[navLinks.length] = el }"
+        class="px-8 py-8 border-t border-snow/8"
+        style="opacity: 0"
+      >
+        <p class="text-snow/35 text-xs font-semibold uppercase tracking-[0.2em] mb-3">
+          Contact
+        </p>
+        <a
+          href="tel:+17058886114"
+          class="text-snow/60 hover:text-persian text-sm font-medium transition-colors duration-200"
+        >
+          +1 (705) 888-6114
+        </a>
+      </div>
+    </div>
+  </Teleport>
+</template>
